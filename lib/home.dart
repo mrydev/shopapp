090de01d276f48bacc669b668d83afd1a4597e41
Emre Yavuz <khapnols@proton.me>
2022-11-30 23:40:04 +0300

    Initial commit

--- lib/home.dart
+++ lib/home.dart
@@ -22,24 +22,68 @@ class _MyHomePageState extends State<MyHomePage> {
     for (var eachItem in jsonData) {
       final items = shopitems(
         title: eachItem['title'],
+        image: eachItem['image'],
+        category: eachItem['category'],
+        price: eachItem['price'].toDouble(),
       );
+
       this.items.add(items);
     }
     print(items.length);
+    print(items[0].price);
   }
 
   @override
   Widget build(BuildContext context) {
     getItems();
     return Scaffold(
+      backgroundColor: Colors.grey.shade400,
       body: FutureBuilder(
         future: getItems(),
         builder: ((context, snapshot) {
           //loading bittiyse
           if (snapshot.connectionState == ConnectionState.done) {
             return ListView.builder(itemBuilder: ((context, index) {
-              return ListTile(
-                title: Text(items[index].title.toString()),
+              return Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: SizedBox(
+                  width: 200,
+                  height: 500,
+                  child: Card(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16)),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(top: 32, bottom: 64.0),
+                            child: ClipRRect(
+                                borderRadius: BorderRadius.circular(16),
+                                child: Image.network(
+                                  items[index].image ?? "",
+                                  height: 300,
+                                  width: 200,
+                                )),
+                          ),
+                          ListTile(
+                            title: Text(
+                              items[index].title ?? "",
+                              style: const TextStyle(
+                                  color: Colors.black, fontSize: 18),
+                            ),
+                            leading: Text(
+                              items[index].category!.toUpperCase(),
+                              style: const TextStyle(color: Colors.indigo),
+                            ),
+                            subtitle: Text(
+                              "\$ ${items[index].price}",
+                              style: const TextStyle(
+                                  color: Colors.black, fontSize: 24),
+                            ),
+                          )
+                        ],
+                      )),
+                ),
               );
             }));
           } else {
